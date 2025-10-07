08d46cc6fc71f30d5bceb2a0d57e7fca470daa54
Danilo Devald <danilo.devald18@gmail.com>
2025-10-06 21:11:56 -0300

    feat(config): configure TypeORM, env validation, and migrations setup

---
+++ src/config/env.validation.ts
@@ -0,0 +1,68 @@
+import { plainToInstance } from 'class-transformer';
+import {
+  IsEnum,
+  IsNumber,
+  IsString,
+  validateSync,
+  IsNotEmpty,
+} from 'class-validator';
+
+enum Environment {
+  Development = 'development',
+  Production = 'production',
+  Test = 'test',
+}
+
+class EnvironmentVariables {
+  @IsEnum(Environment)
+  NODE_ENV: Environment;
+
+  @IsString()
+  @IsNotEmpty()
+  DATABASE_HOST: string;
+
+  @IsNumber()
+  DATABASE_PORT: number;
+
+  @IsString()
+  @IsNotEmpty()
+  DATABASE_USER: string;
+
+  @IsString()
+  @IsNotEmpty()
+  DATABASE_PASSWORD: string;
+
+  @IsString()
+  @IsNotEmpty()
+  DATABASE_NAME: string;
+
+  @IsString()
+  @IsNotEmpty()
+  JWT_SECRET: string;
+
+  @IsString()
+  @IsNotEmpty()
+  JWT_ACCESS_EXPIRATION: string;
+
+  @IsString()
+  @IsNotEmpty()
+  JWT_REFRESH_EXPIRATION: string;
+
+  @IsString()
+  @IsNotEmpty()
+  SWAPI_BASE_URL: string;
+}
+
+export function validate(config: Record<string, unknown>) {
+  const validatedConfig = plainToInstance(EnvironmentVariables, config, {
+    enableImplicitConversion: true,
+  });
+  const errors = validateSync(validatedConfig, {
+    skipMissingProperties: false,
+  });
+
+  if (errors.length > 0) {
+    throw new Error(errors.toString());
+  }
+  return validatedConfig;
+}
