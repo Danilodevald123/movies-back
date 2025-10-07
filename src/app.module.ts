1437970ff7d603633c905d3a13531cdaef58523b
Danilo Devald <danilo.devald18@gmail.com>
2025-10-07 20:42:16 -0300

    Revert "Merge pull request #3 from Danilodevald123/feature/movies-crud"
    This reverts commit 2ad7b168b82bd795682b08f89090011944fee6e3, reversing
    changes made to a65b4fd4e49b003f004f04ca44e764471d6f0020.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,44 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { APP_GUARD, APP_PIPE } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { validate } from './config/env.validation';
-import { getDatabaseConfig } from './config/database.config';
-import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
-import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
-import { MoviesModule } from './movies/movies.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      validate,
-      envFilePath: '.env',
-    }),
-    TypeOrmModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) =>
-        getDatabaseConfig(configService),
-    }),
-    UsersModule,
-    AuthModule,
-    MoviesModule,
-  ],
+  imports: [],
   controllers: [AppController],
-  providers: [
-    AppService,
-    {
-      provide: APP_GUARD,
-      useClass: JwtAuthGuard,
-    },
-    {
-      provide: APP_PIPE,
-      useClass: ValidationPipe,
-    },
-  ],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/auth/interfaces/jwt-user.interface.ts
+++
@@ -1,7 +0,0 @@
-import { UserRole } from '../../users/entities/user.entity';
-
-export interface JwtUser {
-  id: string;
-  email: string;
-  role: UserRole;
-}
